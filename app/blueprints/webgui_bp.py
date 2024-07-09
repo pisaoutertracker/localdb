be6ddbc189dc9f808c50a264dc6d6c7c04e8017a
arizzi <Andrea.Rizzi@cern.ch>
2024-07-09 08:34:57 +0200

    made a usable logbook

--- app/blueprints/webgui_bp.py
+++ app/blueprints/webgui_bp.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+#send_from_directory is used to send files from a directory
+from flask import send_from_directory
 
 bp = Blueprint('main', __name__)
 
@@ -13,3 +15,13 @@ def connectGUI():
 @bp.route('/disconnect_cables.html')
 def disconnectGUI():
     return render_template('disconnect_cables.html')
+
+@bp.route('/logbook.html')
+def logbookGUI():
+    return render_template('logbook.html')
+
+#add route to fetch attachments from /attachments folder
+
+@bp.route('/attachments/<filename>')
+def getAttachment(filename):
+    return send_from_directory("/attachments", filename)
