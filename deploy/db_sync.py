2505441b586fb6f67c8f8598c0c719ea89b1dd81
fvaselli <francesco.vaselli@protonmail.com>
2024-09-06 11:02:11 +0200

    improved version greatly reducing the number of queries to central db for speedup

--- deploy/db_sync.py
+++ deploy/db_sync.py
@@ -3,15 +3,11 @@ import csv
 import io
 import requests
 from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
 import logging
 
-CONFIG_NAME = "test"
-load_dotenv(f"../config/{CONFIG_NAME}.env")
-
 # Constants
-API_URL = "http://192.168.0.45:5005"
+API_URL = os.environ["API_URL"]
 MONGO_URI = os.environ["MONGO_URI"]
 DB_NAME = os.environ["MONGO_DB_NAME"]
 
@@ -34,6 +30,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 def run_rhapi_command(command):
     result = subprocess.run(command, capture_output=True, text=True, shell=True)
+    if result.returncode != 0:
+        logging.error(f"Command failed: {command}\n{result.stderr}")
     return result.stdout
 
 def parse_csv_output(output):
@@ -49,129 +47,137 @@ def get_local_modules():
     req = requests.get(f"{API_URL}/modules")
     return req.json()
 
-def get_module_children(parent_name_label):
-    command = f"""python3 rhapi.py --url=https://cmsdca.cern.ch/trk_rhapi "select * from trker_cmsr.trkr_relationships_v p where p.parent_name_label='{parent_name_label}'" -n --login"""
+def get_children_of_modules(parent_labels):
+    labels = "', '".join(parent_labels)
+    command = f"""python3 rhapi.py --url=https://cmsdca.cern.ch/trk_rhapi "select * from trker_cmsr.trkr_relationships_v r where r.parent_name_label in ('{labels}')" --all --login -n"""
     output = run_rhapi_command(command)
     return parse_csv_output(output)
 
-def get_component_details(component_type, serial_number):
+def get_component_details_in_bulk(component_type, identifiers):
     if component_type not in PARTS_TABLES:
         logging.warning(f"Unknown component type: {component_type}")
-        return None
+        return {}
+    # print(component_type, identifiers)
     table = PARTS_TABLES[component_type]
-    command = f"""python3 rhapi.py --url=https://cmsdca.cern.ch/trk_rhapi "select * from trker_cmsr.{table} p where p.serial_number='{serial_number}'" --all --login -n"""
+    id_field = "NAME_LABEL" if component_type == "MaPSA" or component_type == "PS-s Sensor" or component_type == "PS-p Sensor" or component_type == "MPA Chip" else "SERIAL_NUMBER"
+    ids = "', '".join(identifiers)
+    # print(ids)
+    command = f"""python3 rhapi.py --url=https://cmsdca.cern.ch/trk_rhapi "select * from trker_cmsr.{table} p where p.{id_field} in ('{ids}')" --all --login -n"""
     output = run_rhapi_command(command)
     details = parse_csv_output(output)
-    return details[0] if details else None
+    if component_type == "MaPSA":
+        # MaPSA has two children: PS-p Sensor and MPA Chip
+        # for all MaPSA components, fetch the information of their children with a children query
+        children = get_children_of_modules(identifiers)
+        mapsa_children_map = {}
+        for child in children:
+            parent = child["PARENT_NAME_LABEL"]
+            mapsa_children_map.setdefault(parent, []).append(child)
+            
+        children_details = get_all_component_details(children)
+        for detail in details:
+            cid = detail["NAME_LABEL"]
+            detail["children"] = process_children(mapsa_children_map.get(cid, []), children_details)
+        
+    return {detail[id_field]: detail for detail in details}
 
-def get_module_identifier(module):
-    if "SERIAL_NUMBER" in module:
-        return module["SERIAL_NUMBER"]
-    elif "CHILD_SERIAL_NUMBER" in module and module["CHILD_SERIAL_NUMBER"]:
-        return module["CHILD_SERIAL_NUMBER"]
-    elif "CHILD_NAME_LABEL" in module and module["CHILD_NAME_LABEL"]:
-        return module["CHILD_NAME_LABEL"]
-    elif "CHILD_COMPONENT" in module and "CHILD_ID" in module:
-        return f"{module['CHILD_COMPONENT']}_{module['CHILD_ID']}"
-    else:
-        logging.warning(f"Unable to determine identifier for module: {module}")
-        return None
-
-def get_component_details(component_type, identifier):
-    if component_type not in PARTS_TABLES:
-        logging.warning(f"Unknown component type: {component_type}")
-        return None
-    table = PARTS_TABLES[component_type]
-    # Use NAME_LABEL for MaPSA, SERIAL_NUMBER for others
-    id_field = "name_label" if component_type == "MaPSA" else "serial_number"
-    command = f"""python3 rhapi.py --url=https://cmsdca.cern.ch/trk_rhapi "select * from trker_cmsr.{table} p where p.{id_field}='{identifier}'" --all --login -n"""
-    output = run_rhapi_command(command)
-    details = parse_csv_output(output)
-    return details[0] if details else None
-
-def process_children(parent_name_label):
-    children = get_module_children(parent_name_label)
-    processed_children = []
+def get_all_component_details(children):
+    components_by_type = {}
     for child in children:
-        child_type = child["CHILD_COMPONENT"]
-        child_identifier = child["CHILD_SERIAL_NUMBER"] if child["CHILD_SERIAL_NUMBER"] else child["CHILD_NAME_LABEL"]
-        logging.debug("Child type: ", child_type)
-        logging.debug("Child identifier: ", child_identifier)
-        if not child_identifier:
-            logging.warning(f"Skipping child due to missing identifier: {child}")
+        ctype = child["CHILD_COMPONENT"]
+        if ctype == "PS-s Sensor" or ctype == "MaPSA" or ctype == "PS-p Sensor" or ctype == "MPA Chip":
+            cid = child["CHILD_NAME_LABEL"]
+        else:
+            cid = child["CHILD_SERIAL_NUMBER"]
+        if not cid:
+            raise ValueError(f"Missing identifier for child: {child}")
+        if ctype and cid:
+            components_by_type.setdefault(ctype, set()).add(cid)
+    all_details = {}
+    for ctype, ids in components_by_type.items():
+        # print(ctype, ids)
+        details = get_component_details_in_bulk(ctype, ids)
+        all_details.update(details)
+    return all_details
+
+
+def process_children(children, all_component_details):
+    processed = []
+
+    for child in children:
+        ctype = child["CHILD_COMPONENT"]
+        if ctype == "PS-s Sensor" or ctype == "MaPSA" or ctype == "PS-p Sensor" or ctype == "MPA Chip":
+            cid = child["CHILD_NAME_LABEL"]
+        else:
+            cid = child["CHILD_SERIAL_NUMBER"]
+        if not cid:
+            logging.warning(f"Missing identifier for child: {child}")
             continue
-        child_details = get_component_details(child_type, child_identifier)
-        if child_details:
+
+        details = all_component_details.get(cid)
+        if details:
             child_doc = {
-                "childName": child_identifier,
-                "childType": child_type,
-                "details": child_details,
-                "children": []
+                "childName": cid,
+                "childType": ctype,
+                "details": details,
+                # "children": []  # Initially empty, to be filled if it's MaPSA
             }
-            if child_type == "MaPSA":
-                print("Processing MaPSA children")
-                child_doc["children"] = process_children(child_identifier)
-            processed_children.append(child_doc)
-    return processed_children
+            processed.append(child_doc)
 
-def process_module(module, mongo_collection):
-    module_identifier = get_module_identifier(module)
-    if not module_identifier:
-        logging.error(f"Skipping module due to missing identifier: {module}")
-        return None
+    return processed
 
-    logging.info(f"Processing module {module_identifier}...")
-
+def process_module(module, children_map, all_component_details, mongo_collection):
+    module_id = module.get("SERIAL_NUMBER")
+    if not module_id:
+        logging.error(f"No SERIAL_NUMBER for module: {module}")
+        return
+    children = children_map.get(module["NAME_LABEL"], [])
     module_doc = {
-        "moduleName": module_identifier,
+        "moduleName": module_id,
         "details": module,
-        "children": []
+        "children": process_children(children, all_component_details)
     }
-
-    if "NAME_LABEL" in module:
-        module_doc["children"] = process_children(module["NAME_LABEL"])
-
-    # Insert top-level module into MongoDB
+    # print(module_doc)
     mongo_collection.update_one(
-        {"moduleName": module_doc["moduleName"]},
+        {"moduleName": module_id},
         {"$set": module_doc},
         upsert=True
     )
 
-    return module_doc
-
 def main():
-    # Connect to MongoDB
     client = MongoClient(MONGO_URI)
     db = client[DB_NAME]
     modules_collection = db["modules"]
     logging.info("Connected to MongoDB.")
     
-    # remove all documents from the collection
     # modules_collection.delete_many({})
-    # Print all modules
-    for module in modules_collection.find():
-        print(module)
-
-    # Get modules from central and local DBs
+    
     central_modules = get_central_modules()
     local_modules = get_local_modules()
-
+    
     logging.info(f"Central DB has {len(central_modules)} modules.")
     logging.info(f"Local DB has {len(local_modules)} modules.")
-
-    # Find missing modules
-    local_module_names = set(module["moduleName"] for module in local_modules)
-    missing_modules = [module for module in central_modules if module["SERIAL_NUMBER"] not in local_module_names]
-    logging.info(f"Found {len(missing_modules)} missing modules.")
-
-    # Process and insert missing modules
-    processed_modules = 0
-    for module in missing_modules:
-        process_module(module, modules_collection)
-        processed_modules += 1
-
-    logging.info(f"Sync completed. {processed_modules} top-level modules added or updated.")
+    
+    local_names = set(m["moduleName"] for m in local_modules)
+    missing = [m for m in central_modules if m["SERIAL_NUMBER"] not in local_names]
+    logging.info(f"Missing modules: {len(missing)}")
+    
+    parent_labels = [m["NAME_LABEL"] for m in missing]
+    children = get_children_of_modules(parent_labels)
+    # print(children)
+    
+    all_details = get_all_component_details(children)
+    
+    # Organize children by parent
+    children_map = {}
+    for child in children:
+        parent = child["PARENT_NAME_LABEL"]
+        children_map.setdefault(parent, []).append(child)
+    
+    for module in missing:
+        process_module(module, children_map, all_details, modules_collection)
+    
+    logging.info("Sync completed.")
 
 if __name__ == "__main__":
     main()
