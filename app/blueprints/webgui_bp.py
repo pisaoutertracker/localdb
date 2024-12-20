645cf297c11f88d1f3dae7c7229ab1c7e3d02d88
arizzi <Andrea.Rizzi@cern.ch>
2024-12-20 20:30:47 +0100

    use static for static

--- app/blueprints/webgui_bp.py
+++ app/blueprints/webgui_bp.py
@@ -8,27 +8,6 @@ bp = Blueprint('main', __name__)
 def homeGUI():
     return render_template('home.html')
 
-@bp.route('/connect_cables.html')
-def connectGUI():
-    return render_template('connect_cables.html')
-
-@bp.route('/disconnect_cables.html')
-def disconnectGUI():
-    return render_template('disconnect_cables.html')
-
-@bp.route('/insert_cables.html')
-def insertGUI():
-    return render_template('insert_cables.html')
-
-@bp.route('/logbook.html')
-def logbookGUI():
-    return render_template('logbook.html')
-
-@bp.route('/connections.html')
-def connectionsGUI():
-    return render_template('connections.html')
-#add route to fetch attachments from /attachments folder
-
 @bp.route('/attachments/<filename>')
 def getAttachment(filename):
     return send_from_directory("/attachments", filename)
