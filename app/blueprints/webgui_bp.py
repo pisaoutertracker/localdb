2d0274237f058fcc524146c1cf46cecd180a11d5
arizzi <Andrea.Rizzi@cern.ch>
2025-10-10 23:36:09 +0200

    improve page navigation

--- app/blueprints/webgui_bp.py
+++ app/blueprints/webgui_bp.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template
-#send_from_directory is used to send files from a directory
-from flask import send_from_directory
+# send_from_directory is used to send files from a directory
+from flask import send_from_directory, current_app
 
 bp = Blueprint('main', __name__)
 
+
 @bp.route('/')
 def homeGUI():
-    return render_template('home.html')
+    # Serve the static index.html so the home page can be a static file
+    return send_from_directory(current_app.static_folder, 'index.html')
+
 
 @bp.route('/attachments/<filename>')
 def getAttachment(filename):
