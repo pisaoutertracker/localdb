f651b26cb65a062eedfb9a1a5362afa8b6e3d694
fvaselli <francesco.vaselli@protonmail.com>
2025-11-18 22:00:09 +0100

    refactor static/analysis scripts to accept config.js. add two more scripts

--- app/blueprints/webgui_bp.py
+++ app/blueprints/webgui_bp.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 # send_from_directory is used to send files from a directory
 from flask import send_from_directory, current_app
+import os
 
 bp = Blueprint('main', __name__)
 
@@ -14,3 +15,17 @@ def homeGUI():
 @bp.route('/attachments/<filename>')
 def getAttachment(filename):
     return send_from_directory("/attachments", filename)
+
+
+@bp.route('/session_testing_flow')
+def session_testing_flow():
+    # Serve the session testing flow HTML page from the static/analysis folder
+    analysis_dir = os.path.join(current_app.static_folder, 'analysis')
+    return send_from_directory(analysis_dir, 'session_testing_flow.html')
+
+
+@bp.route('/session_results')
+def session_results():
+    # Serve the session results HTML page from the static/analysis folder
+    analysis_dir = os.path.join(current_app.static_folder, 'analysis')
+    return send_from_directory(analysis_dir, 'session_results.html')
