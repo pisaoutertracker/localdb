3287ff633fd9d4cd9ebaa66f8817341ed11694cd
fvaselli <francesco.vaselli@protonmail.com>
2026-06-26 16:50:32 +0200

    change location field check to match what's actually updated

--- app/blueprints/modules_on_ring.py
+++ app/blueprints/modules_on_ring.py
@@ -12,7 +12,7 @@ bp = Blueprint("modules_on_ring", __name__)
 def get_modules_on_ring():
     modules_collection = get_db()["modules"]
     cursor = modules_collection.find(
-        {"status": "MOUNTED", "Current Center": "IT-Pisa[INFN Pisa]"},
+        {"status": "MOUNTED", "details.LOCATION": "IT-Pisa[INFN Pisa]"},
         # get also the LOCATION attributre living under "details"
         {"_id": 0, "moduleName": 1, "mounted_on": 1, "details.LOCATION": 1},
     )
